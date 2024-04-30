53d063deec4a358f607b6cafd6860189d9fae945
manu <whiteelement1991@web.de>
2024-04-30 11:11:23 +0200

    - started working on menus

--- manager.go
+++ manager.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"encoding/xml"
+	"fmt"
 	"log"
 	"os"
 )
@@ -24,32 +26,70 @@ type Player struct {
 	Name string `xml:"name"`
 }
 
-func readXml(players *[]Player, npc *[]Npc) {
+func readXml(players *XMLPlayers, npcs *XMLNpcs) {
 	data, err := os.Open("data/entities.xml")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	var parsedPlayers XMLPlayers
-	var parsedNpcs XMLNpcs
-	err = xml.NewDecoder(data).Decode(&parsedPlayers)
+	err = xml.NewDecoder(data).Decode(&players)
 	_, err = data.Seek(0, 0)
 
 	if err != nil {
 		return
 	}
 
-	err = xml.NewDecoder(data).Decode(&parsedNpcs)
+	err = xml.NewDecoder(data).Decode(npcs)
 
 	if err != nil {
 		return
 	}
 }
 
+func printEntities(players XMLPlayers, npcs XMLNpcs) {
+	fmt.Println("Players\n===============")
+	for i, p := range players.Players {
+		fmt.Printf("%v: %s (Ini: %v)\n", i+1, p.Name, p.Init)
+	}
+
+	fmt.Println("'\n\nNPCs\n===============")
+	for i, p := range npcs.Npcs {
+		fmt.Printf("%v: %s (Ini: %v)\n", i+1, p.Name, p.Init)
+	}
+}
+
+func choseEntities(players *XMLPlayers, npcs *XMLNpcs) {
+	fmt.Println("Welche Spieler sollen am Kampf teilnehmen?")
+	fmt.Println("1. Alle Spieler")
+	fmt.Println("2. Auswählen")
+
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+
+	switch scanner.Text() {
+	case "1":
+		//TODO npcs auswählen
+		fmt.Println("NPC auswählen screen")
+	case "2":
+		//TODO Subset der Player auswählen
+		fmt.Println("Subset der Player auswählen")
+	}
+}
+
 func main() {
-	npcs := []Npc{}
-	players := []Player{}
+	npcs := XMLNpcs{}
+	players := XMLPlayers{}
 
 	readXml(&players, &npcs)
+	printEntities(players, npcs)
 
+	fmt.Println("\n1. Alten Kampf fortsetzen\n2. Neuen Kampf starten")
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	switch scanner.Text() {
+	case "1":
+		fmt.Println("Continuing fight not yet implemented")
+	case "2":
+		choseEntities(&players, &npcs)
+	}
 }
